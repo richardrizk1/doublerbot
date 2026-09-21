913086cdb1017ce89e050f55f78ddcf52fe60d5e
richardrizk1 <Richard-rizk@hotmail.com>
2026-09-21 23:40:03 +0300

    Update DoubleRcarsbot.py

--- DoubleRcarsbot.py
+++ DoubleRcarsbot.py
@@ -10,7 +10,7 @@ def download_file_from_google_drive(id, destination):
     if os.path.exists(destination):
         os.remove(destination)
     url = f"https://drive.google.com/uc?id={id}"
-    gdown.download(url, destination, quiet=False)
+    gdown.download(id=id, ouput=destination, quiet=False)
     print("Download finished!")
 
 if not os.path.exists("CARMDI.csv"):
